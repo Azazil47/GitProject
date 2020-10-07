80587df667206d7e8d176c645b6c86a060c577ba
Azazil47 <72367218+Azazil47@users.noreply.github.com>
2020-10-07 18:22:43 +0700

    #TDP-1 Добавление класса
    И добавление файла
    И другая хуета

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitProject
+{
+    class Class1
+    {
+        public static String Lol;
+        public int i;
+        public String line;
+    }
+}
